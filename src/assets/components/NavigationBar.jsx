e1d47e68642551a43c80d4fa5d78f8b41aa13356
iamfaisalpk <iamfaizy123@gmail.com>
2025-02-20 09:53:15 +0530

    not working

--- src/assets/components/NavigationBar.jsx
+++ src/assets/components/NavigationBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import classes from '../components/'
 
 const NavigationBar = () => {
 return (
     <>
-        <header>
+        <header className={classes.header}>
             <nav>
                 <ul>
                     <li><Link to='/'>Home</Link></li>
