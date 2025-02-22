8ec62874ea0e0dfd078abe5933cd3faa43134ab2
iamfaisalpk <iamfaizy123@gmail.com>
2025-02-22 15:00:55 +0530

    changing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import {createBrowserRouter , RouterProvider} from 'react-router-dom';
 import RootLayout from './assets/components/Root.Layout';
+import Error from './assets/components/Error';
 import Routing from './assets/components/Router';
 import Products from './assets/components/Products';
 import Difining from './assets/components/Products,Difining';
-import Error from './assets/components/Error';
 
-
-const router =  createBrowserRouter ([
-  {path : '', element : <RootLayout/> ,errorElement : <Error/>, children :[
-    {path : '', element : <Routing/>},
-    {path : '/products', element : <Products/>},
-    {path : '/products/:productid', element : <Difining/>},
+const router = createBrowserRouter([
+  {path : '', element : <RootLayout/>, errorElement : <Error/>, children:[
+    { path : '', element: <Routing/>},
+    { path : '/products', element: <Products/>},
+    { path : '/products/:productid', element: <Difining/>},
   ]}
 ])
 const App = () => { 
