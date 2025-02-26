1405b4ed41fda539b4a98d407334b0adff88585c
iamfaisalpk <iamfaizy123@gmail.com>
2025-02-26 15:56:22 +0530

    changing evrythink

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import {createBrowserRouter , RouterProvider} from 'react-router-dom';
 import RootLayout from './assets/components/Root.Layout';
+import Error from './assets/components/Error';
+import Routing from './assets/components/Router';
+import Products from './assets/components/Products';
+import Difining from './assets/components/Products,Difining';
 
+const router = createBrowserRouter([
+  {path : '', element : <RootLayout/>, errorElement : <Error/>, children:[
+    { path : '', element: <Routing/>},
+    { path : '/products', element: <Products/>},
+    { path : '/products/:productid', element: <Difining/>},
+  ]}
+])
 const App = () => { 
   return (
     <>
